9d90e656f3197c4ecbb33d132320c4ff9eed4b2b
taku0625 <takutaku06250625@gmail.com>
2022-11-01 20:35:13 +0900

    add: load param

--- ball_tracking/track_ball.py
+++ ball_tracking/track_ball.py
@@ -1,15 +1,17 @@
 import cv2
+import numpy as np
 
 from ball_tracker.tracker_by_color import TrackerByColor
 
 
 def main():
-    MIN_HSV = [0, 130, 100]  # [150, 50, 200]
-    MAX_HSV = [88, 165, 255]  # [180, 255, 255]
-    THRESHOLD = 50
+    param_for_tracking_by_color = np.load("bin/param_for_tracking_by_color.npz", allow_pickle=True)
+    min_hsv = param_for_tracking_by_color["min_hsv"]  # [0, 130, 100]  # [150, 50, 200]
+    max_hsv = param_for_tracking_by_color["max_hsv"]  # [88, 165, 255]  # [180, 255, 255]
+    threshold = float(param_for_tracking_by_color["threshold"])  # 50
 
     cap = cv2.VideoCapture(0)
-    tracker = TrackerByColor(MIN_HSV, MAX_HSV, THRESHOLD)
+    tracker = TrackerByColor(min_hsv, max_hsv, threshold)
 
     while True:
         _, frame = cap.read()
