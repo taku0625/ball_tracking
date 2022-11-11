c38b07a3309404782ea91099d929716242f89f6e
taku0625 <takutaku06250625@gmail.com>
2022-11-11 15:42:18 +0900

    add: save video

--- ball_tracking/track_ball.py
+++ ball_tracking/track_ball.py
@@ -11,19 +11,41 @@ def main():
     threshold = float(param_for_tracking_by_color["threshold"])  # 50
 
     cap = cv2.VideoCapture(0)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    writer = cv2.VideoWriter(
+        "bin/draw_trajectory.avi",
+        fourcc,
+        fps,
+        (width, height)
+    )
+
     tracker = TrackerByColor(min_hsv, max_hsv, threshold)
+
+    do_save_video = False
 
     while True:
         _, frame = cap.read()
 
-        frame = tracker.draw_trajectory(frame)
-
         cv2.imshow("image", frame)
 
-        if cv2.waitKey(30) & 0xFF == ord("q"):
+        if do_save_video:
+            writer.write(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+        elif cv2.waitKey(1) & 0xFF == ord("s"):
+            frame = tracker.draw_trajectory(frame)
+            do_save_video = True
+            print("Saving video")
+        elif cv2.waitKey(1) & 0xFF == ord("f"):
+            pass
 
     cap.release()
+    writer.release()
     cv2.destroyAllWindows()
 
 
