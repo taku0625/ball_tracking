6fafafbc5a9b383e2841621bc69100b5fd2ca01f
taku0625 <takutaku06250625@gmail.com>
2023-01-12 16:52:27 +0900

    clean: hsv_track_bar.py

--- ball_tracking/analysis_tools/hsv_track_bar.py
+++ ball_tracking/analysis_tools/hsv_track_bar.py
@@ -5,7 +5,7 @@ from const.hsv_idx import HUE_IDX, SATURATION_IDX, VALUE_IDX
 
 
 class HSVTrackBar:
-    def __init__(self, initial_min_hsv, initial_max_hsv, initial_threshold):
+    def __init__(self, initial_min_hsv, initial_max_hsv, initial_binary_threshold):
         def nothing(x):
             pass
 
@@ -19,56 +19,35 @@ class HSVTrackBar:
         cv2.createTrackbar("maxS", "track_bar", initial_max_hsv[SATURATION_IDX], 255, nothing)
         cv2.createTrackbar("maxV", "track_bar", initial_max_hsv[VALUE_IDX], 255, nothing)
 
-        cv2.createTrackbar("threshold", "track_bar", initial_threshold, 100, nothing)
+        cv2.createTrackbar("binary_threshold", "track_bar", initial_binary_threshold, 100, nothing)
 
-    @property
-    def param_for_tracking_by_color(self):
-        self.__set_param_for_tracking_by_color_to_track_bar_value()
-        return self._min_hsv, self._max_hsv, self._threshold
-
-    @property
-    def min_hsv(self):
-        self.__set_min_hsv_to_track_bar_value()
-        return self._min_hsv
-
-    @property
-    def max_hsv(self):
-        self.__set_max_hsv_to_track_bar_value()
-        return self._max_hsv
-
-    @property
-    def threshold(self):
-        self.__set_threshold_to_track_bar_value()
-        return self._threshold
-
-    def __set_param_for_tracking_by_color_to_track_bar_value(self):
-        self.__set_min_hsv_to_track_bar_value()
-        self.__set_max_hsv_to_track_bar_value()
-        self.__set_threshold_to_track_bar_value()
-
-    def __set_min_hsv_to_track_bar_value(self):
-        self._min_hsv = (
+    def get_min_hsv(self):
+        return(
             cv2.getTrackbarPos("minH", "track_bar"),
             cv2.getTrackbarPos("minS", "track_bar"),
             cv2.getTrackbarPos("minV", "track_bar"),
         )
 
-    def __set_max_hsv_to_track_bar_value(self):
-        self._max_hsv = (
+    def get_max_hsv(self):
+        return (
             cv2.getTrackbarPos("maxH", "track_bar"),
             cv2.getTrackbarPos("maxS", "track_bar"),
             cv2.getTrackbarPos("maxV", "track_bar"),
         )
 
-    def __set_threshold_to_track_bar_value(self):
-        self._threshold = cv2.getTrackbarPos("threshold", "track_bar")
+    def get_binary_threshold(self):
+        return cv2.getTrackbarPos("binary_threshold", "track_bar")
 
-    def save_param_for_tracking_by_color(self, save_path):
-        self.__set_param_for_tracking_by_color_to_track_bar_value()
+    def save_track_bar_values(self, save_path):
+        min_hsv = self.get_min_hsv()
+        max_hsv = self.get_max_hsv()
+        binary_threshold = self.get_binary_threshold()
+
         np.savez(
             save_path,
-            min_hsv=self._min_hsv,
-            max_hsv=self._max_hsv,
-            threshold=self._threshold,
+            min_hsv=min_hsv,
+            max_hsv=max_hsv,
+            binary_threshold=binary_threshold,
         )
+
         print("This param for tracking by color was saved.")
