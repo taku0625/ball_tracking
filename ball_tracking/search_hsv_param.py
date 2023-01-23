e0534b98845cf28cfc032717ec94f8f3d874568d
taku0625 <takutaku06250625@gmail.com>
2023-01-23 12:44:13 +0900

    clean: execution file

--- ball_tracking/search_hsv_param.py
+++ ball_tracking/search_hsv_param.py
@@ -5,49 +5,57 @@ from analysis_tools.hsv_track_bar import HSVTrackBar
 from const.pkg_path import BASE_VIDEO_DIR_HOME, HSV_PARAM_DIR_HOME
 
 
-def main():
-    PROJECT_NAME = "Design_and_Dragting_No3"
-    BASE_VIDEO_PATH = f"{BASE_VIDEO_DIR_HOME}\\{PROJECT_NAME}\\competitive.mp4"
-    SAVE_HSV_PARAM_PATH = f"{HSV_PARAM_DIR_HOME}\\ball_hsv_param.npz"
+class SearchHSVParam:
+    def __init__(self, project_name, base_video_file_name, save_hsv_param_file_name):
+        BASE_VIDEO_PATH = f"{BASE_VIDEO_DIR_HOME}\\{project_name}\\{base_video_file_name}"
+        SAVE_HSV_PARAM_PATH = f"{HSV_PARAM_DIR_HOME}\\{save_hsv_param_file_name}"
 
-    MIN_HSV = [0, 180, 100]  # [150, 50, 200]
-    MAX_HSV = [255, 255, 255]  # [180, 255, 255]
-    BINARY_THRESHOLD = 50
+        MIN_HSV = [0, 180, 100]  # [150, 50, 200]
+        MAX_HSV = [255, 255, 255]  # [180, 255, 255]
+        BINARY_THRESHOLD = 50
 
-    cap = cv2.VideoCapture(BASE_VIDEO_PATH)
-    hsv_processor = HSVProcessor(MIN_HSV, MAX_HSV, BINARY_THRESHOLD)
-    track_bar = HSVTrackBar(MIN_HSV, MAX_HSV, BINARY_THRESHOLD)
+        self._cap = cv2.VideoCapture(BASE_VIDEO_PATH)
+        self._hsv_processor = HSVProcessor(MIN_HSV, MAX_HSV, BINARY_THRESHOLD)
+        self._track_bar = HSVTrackBar(MIN_HSV, MAX_HSV, BINARY_THRESHOLD)
+        
+        while True:
+            grabbed, frame = self._cap.read()
+            if not grabbed:
+                self._cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                continue
 
-    while True:
-        grabbed, frame = cap.read()
-        if not grabbed:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            continue
+            self.__hsv_process(frame)
 
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            elif cv2.waitKey(1) & 0xFF == ord("s"):
+                self._track_bar.save_track_bar_values(SAVE_HSV_PARAM_PATH)
+                print("The param is saved.")
+        self.__finish()
+
+    def __hsv_process(self, frame):
         # pipeline between track_bar and hsv_processor
-        min_hsv = track_bar.get_min_hsv()
-        max_hsv = track_bar.get_max_hsv()
-        binary_threshold = track_bar.get_binary_threshold()
-        hsv_processor.min_hsv = min_hsv
-        hsv_processor.max_hsv = max_hsv
-        hsv_processor.binary_threshold = binary_threshold
+        min_hsv = self._track_bar.get_min_hsv()
+        max_hsv = self._track_bar.get_max_hsv()
+        binary_threshold = self._track_bar.get_binary_threshold()
+        self._hsv_processor.min_hsv = min_hsv
+        self._hsv_processor.max_hsv = max_hsv
+        self._hsv_processor.binary_threshold = binary_threshold
 
-        mask_frame = hsv_processor.generate_mask(frame)
-        out_line_info = hsv_processor.find_outline_of_rect(frame)
-        cut_frame = hsv_processor.draw_outline_of_rect(frame, out_line_info)
+        mask_frame = self._hsv_processor.generate_mask(frame)
+        out_line_info = self._hsv_processor.find_outline_of_rect(frame)
+        cut_frame = self._hsv_processor.draw_outline_of_rect(frame, out_line_info)
 
         cv2.imshow("image", mask_frame)
         cv2.imshow("cut_image", cut_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-        elif cv2.waitKey(1) & 0xFF == ord("s"):
-            track_bar.save_track_bar_values(SAVE_HSV_PARAM_PATH)
-            print("The param is saved.")
-
-    cap.release()
-    cv2.destroyAllWindows()
+    def __finish(self):
+        self._cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    main()
+    PROJECT_NAME = "Design_and_Dragting_No3"
+    BASE_VIDEO_FILE_NAME = "competitive.mp4"
+    SAVE_HSV_PARAM_FILE_NAME = "ball_hsv_param.npz"
+    search_hsv_param = SearchHSVParam(PROJECT_NAME, BASE_VIDEO_FILE_NAME, SAVE_HSV_PARAM_FILE_NAME)
--- ball_tracking/track_ball.py
+++ ball_tracking/track_ball.py
@@ -10,80 +10,97 @@ from const.pkg_path import (
     PROCESSED_VIDEO_DIR_HOME,
     TRAJECTORY_IMAGE_DIR_HOME,
     TRAJECTORY_POINTS_DIR_HOME,
+    TRAJECTORY_POINTS_IMG_DIR_HOME,
 )
 from const.design_and_drafting_no3 import SUPPORT_WIDTH, SUPPORT_HEIGHT
 import matplotlib.pyplot as plt
 
-def main():
-    PROJECT_NAME = "Design_and_Dragting_No3"
-    BASE_VIDEO_DIR = f"{BASE_VIDEO_DIR_HOME}\\{PROJECT_NAME}"
-    PROCESSED_VIDEO_DIR = f"{PROCESSED_VIDEO_DIR_HOME}\\{PROJECT_NAME}"
-    TRAJECTORY_IMAGE_DIR = f"{TRAJECTORY_IMAGE_DIR_HOME}\\{PROJECT_NAME}"
-    TRAJECTORY_POINTS_DIR = f"{TRAJECTORY_POINTS_DIR_HOME}\\{PROJECT_NAME}"
-    TRAJECTORY_POINTS_IMG_DIR = f"{TRAJECTORY_POINTS_IMG_DIR_HOME}\\{PROJECT_NAME}"
-    HSV_PARAM_PATH = f"{HSV_PARAM_DIR_HOME}\\ball_hsv_param.npz"
 
-    ball_hsv_param = np.load(HSV_PARAM_PATH, allow_pickle=True)
-    ball_min_hsv = ball_hsv_param["min_hsv"]  # [0, 130, 100]  # [150, 50, 200]
-    ball_max_hsv = ball_hsv_param["max_hsv"]  # [88, 165, 255]  # [180, 255, 255]
-    ball_binary_threshold = float(ball_hsv_param["binary_threshold"])  # 50
-    
-    base_video_path_list = glob.glob(f"{BASE_VIDEO_DIR}\\*")
+class TrackBall:
+    def __init__(self, project_name, hsv_param_file_name):
+        self._project_name = project_name
+        BASE_VIDEO_DIR = f"{BASE_VIDEO_DIR_HOME}\\{self._project_name}"
+        HSV_PARAM_PATH = f"{HSV_PARAM_DIR_HOME}\\{hsv_param_file_name}"
 
-    for base_video_path in base_video_path_list:
-        base_video_basename = os.path.splitext(os.path.basename(base_video_path))[0]
+        ball_hsv_param = np.load(HSV_PARAM_PATH, allow_pickle=True)
+        self._ball_min_hsv = ball_hsv_param["min_hsv"]  # [0, 130, 100]  # [150, 50, 200]
+        self._ball_max_hsv = ball_hsv_param["max_hsv"]  # [88, 165, 255]  # [180, 255, 255]
+        self._ball_binary_threshold = float(ball_hsv_param["binary_threshold"])  # 50
+        
+        base_video_path_list = glob.glob(f"{BASE_VIDEO_DIR}\\*")
+        for base_video_path in base_video_path_list:
+            self.__prepare_track(base_video_path)
+
+            first_loop = True
+            frame_count = int(self._cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            for _ in range(frame_count):
+                _, frame = self._cap.read()
+
+                if first_loop:
+                    self._x1, self._y1, self._roi_width, self._roi_height = cv2.selectROI("image", frame)
+                    cv2.resizeWindow("cut_image", 1000, int(1000 * self._roi_height / self._roi_width))
+                    first_loop = False
+
+                self.__track(frame)
+
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+            self.__finish_track(frame)
+
+    def __prepare_track(self, base_video_path):
+        PROCESSED_VIDEO_DIR = f"{PROCESSED_VIDEO_DIR_HOME}\\{self._project_name}"
+        self._base_video_basename = os.path.splitext(os.path.basename(base_video_path))[0]
         # get video info
-        cap = cv2.VideoCapture(base_video_path)
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(cap.get(cv2.CAP_PROP_FPS))
-        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self._cap = cv2.VideoCapture(base_video_path)
+        width = int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(self._cap.get(cv2.CAP_PROP_FPS))
+
+        self._base_tracker = TrackerByColor(self._ball_min_hsv, self._ball_max_hsv, self._ball_binary_threshold)
+        self._roi_tracker = TrackerByColor(self._ball_min_hsv, self._ball_max_hsv, self._ball_binary_threshold)
 
         fourcc = cv2.VideoWriter_fourcc(*"XVID")
-
-        base_tracker = TrackerByColor(ball_min_hsv, ball_max_hsv, ball_binary_threshold)
-        roi_tracker = TrackerByColor(ball_min_hsv, ball_max_hsv, ball_binary_threshold)
+        self._writer = cv2.VideoWriter(f"{PROCESSED_VIDEO_DIR}\\{self._base_video_basename}.avi", fourcc, fps, (width, height))
 
         cv2.namedWindow("cut_image", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("cut_image", 1000, int(1000 * height / width))
 
-        first_loop = True
-        for _ in range(frame_count):
-            _, frame = cap.read()
+    def __track(self, frame):
+        roi_frame = frame[self._y1 : self._y1 + self._roi_height, self._x1 : self._x1 + self._roi_width]
+        roi_frame = self._roi_tracker.draw_trajectory(roi_frame)
+        frame = self._base_tracker.draw_trajectory(frame)
+        cv2.imshow("image",frame)
+        cv2.imshow("cut_image",roi_frame)
+        self._writer.write(frame)
 
-            if first_loop:
-                x1, y1, roi_width, roi_height = cv2.selectROI("image", frame)
-                writer = cv2.VideoWriter(
-                    f"{PROCESSED_VIDEO_DIR}\\{base_video_basename}.avi", fourcc, fps, (width, height)
-                )
-                cv2.resizeWindow("cut_image", 1000, int(1000 * roi_height / roi_width))
-                first_loop = False
+    def __finish_track(self, frame):
+        TRAJECTORY_IMAGE_DIR = f"{TRAJECTORY_IMAGE_DIR_HOME}\\{self._project_name}"
+        TRAJECTORY_POINTS_DIR = f"{TRAJECTORY_POINTS_DIR_HOME}\\{self._project_name}"
+        TRAJECTORY_POINTS_IMG_DIR = f"{TRAJECTORY_POINTS_IMG_DIR_HOME}\\{self._project_name}"
 
-            roi_frame = frame[y1 : y1 + roi_height, x1 : x1 + roi_width]
-            roi_frame = roi_tracker.draw_trajectory(roi_frame)
-            frame = base_tracker.draw_trajectory(frame)
-            cv2.imshow("image",frame)
-            cv2.imshow("cut_image",roi_frame)
-            writer.write(frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
-
-        cv2.imwrite(f"{TRAJECTORY_IMAGE_DIR}\\{base_video_basename}.jpeg", frame)
+        cv2.imwrite(f"{TRAJECTORY_IMAGE_DIR}\\{self._base_video_basename}.jpeg", frame)
         roi_trajectory_points = np.array(
             [
-                (x / roi_width * SUPPORT_WIDTH, - y / roi_height * SUPPORT_HEIGHT + SUPPORT_HEIGHT) 
-                for x, y in roi_tracker.trajectory_points
+                (x / self._roi_width * SUPPORT_WIDTH, - y / self._roi_height * SUPPORT_HEIGHT + SUPPORT_HEIGHT)
+                for x, y in self._roi_tracker.trajectory_points
             ]
         )
-        np.save(f"{TRAJECTORY_POINTS_DIR}\\{base_video_basename}.npy", roi_trajectory_points)
+        np.save(f"{TRAJECTORY_POINTS_DIR}\\{self._base_video_basename}.npy", roi_trajectory_points)
 
-        cap.release()
-        writer.release()
+        self._base_tracker.reset_trajectory()
+        self._roi_tracker.reset_trajectory()
+
+        self._cap.release()
+        self._writer.release()
         cv2.destroyAllWindows()
 
         plt.scatter(roi_trajectory_points[:, 0], roi_trajectory_points[:, 1])
-        plt.savefig(f"{TRAJECTORY_POINTS_IMG_DIR}\\{base_video_basename}.png")
+        plt.savefig(f"{TRAJECTORY_POINTS_IMG_DIR}\\{self._base_video_basename}.png")
         plt.show()
 
+
 if __name__ == "__main__":
-    main()
+    PROJECT_NAME = "Design_and_Dragting_No3"
+    HSV_PARAM_FILE_NAME = "ball_hsv_param.npz"
+    track_ball = TrackBall(PROJECT_NAME, HSV_PARAM_FILE_NAME)
